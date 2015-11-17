6a502a79280eea48e63ff6924c79f3274037e1d5
suada abdirahman <suada abdirahman>
2015-11-17 12:27:58 +0300

    Complete the num_to_words.js

--- shared/suada/num_to_words.js
+++ shared/suada/num_to_words.js
@@ -1,5 +1,15 @@
-function num_to_words (num) {
-	var key = ["zero", "one", "two", "three"];
-	
-
+var num_to_words = function(num){
+ var s = num.toString();
+ var length = s.length;
+ var words = " ";
+ var a= ["Zero","One", "Two", "Three", "Four", "Five"];
+ 
+ for(var i = 0; i < length; i++){
+ 
+ words += a[parseInt(s[i])] + " ";
+ 
 }
+return words;
+}
+
+console.log(num_to_words(2005));
