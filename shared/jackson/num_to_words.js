09942b054fd36ee8a8dc5e5ab1db038039cfce77
jackson <ndirangu240@gmail.com>
2015-11-17 12:58:03 +0300

    Add num to words

---
+++ shared/jackson/num_to_words.js
@@ -0,0 +1,21 @@
+var words = ["zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"];
+var num_to_words = function (num){
+    var str_num = num.toString();
+    var num_in_words = "";
+    len = str_num.length;
+    for (var i = 0; i < len; i++){
+       num_in_words = num_in_words.concat(words[parseInt(str_num[i])], " ");
+    // num_in_words += words[parseInt(str_num[i])]+" ";
+    }
+    return num_in_words;
+}
+console.log(num_to_words(2344));
