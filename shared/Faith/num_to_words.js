80a9979b949e683921353918b2588664d5a7ea79
Faith <fnzisa@gmail.com>
2015-11-17 12:32:50 +0300

    Updated file extension

--- shared/Faith/num_to_words.js
+++ shared/Faith/num_to_words.js
@@ -1,5 +1,8 @@
+
 function num_to_words (num){
+
     var ary =["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+    
     var str = num.toString(), len = str.length, words = "";
     
     for(var i=0; i<len; i++){
